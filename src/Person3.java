4a024e621f1a13cfe65c4adba03422676a3dae0c
razvancornita <cornita_razvan@yahoo.co>
2020-04-21 20:15:10 +0300

    - added Person3

---
+++ src/Person3.java
@@ -0,0 +1,23 @@
+public class Person3 {
+
+    public int id;
+    public String name;
+
+    public Person3(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
+//commit -> add Person
+//PE LOCAL
+
+//push
