b8aa0559b0463fa6c2ffe5c532fb277e9b11e4c6
razvancornita <cornita_razvan@yahoo.co>
2020-04-21 20:14:47 +0300

    - added Person and Person1 (not finished)

---
+++ src/Person2.java
@@ -0,0 +1,31 @@
+public class Person2 {
+
+    public int id;
+    public String name;
+
+    public Person2(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+//commit -> add Person
+//PE LOCAL
+
+//push
